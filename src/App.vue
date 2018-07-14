2af6ae3596fe37b4e766c929f4a951f2eca7faa3
Berlin Sky <pedroyoung8989@gmail.com>
2018-07-14 10:51:10 -0700

    Initial setup

---
+++ src/App.vue
@@ -0,0 +1,61 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <h1>{{ msg }}</h1>
+    <h2>Fancy Button</h2>
+
+    <fancy-button @buttonClicked="eventListenner($event)" button-title="Not so fancy Button"></fancy-button>
+
+    <button>Static Button</button>
+  </div>
+</template>
+
+<script>
+import FancyButton from './components/FancyButton';
+
+export default {
+  name: 'app',
+  components: {
+    FancyButton
+  },
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  },
+  methods: {
+    eventListenner(message) {
+      console.log("The button was clicked from the child component with this message: " + message);
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
---
+++ src/components/FancyButton.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <input type="text" v-model="messsage">
+    <button @click.prevent="clicked">
+      {{ buttonTitle }}
+    </button>
+</div>
+</template>
+
+<script>
+export default {
+  props: {
+    buttonTitle: {
+      type: String,
+      default: "Little Fancy Button",
+      required: true,
+      validator: value => value.length > 3
+    }
+  },
+  data() {
+    return {
+      buttonText: 'So called Fancy Button',
+      messsage: ''
+    }
+  },
+  methods: {
+    clicked() {
+      this.$emit('buttonClicked', this.messsage);
+    }
+  }
+}
+</script>
+
+<style scoped>
+  button {
+    border: 1px solid red;
+    padding: 10px;
+  }
+</style>
