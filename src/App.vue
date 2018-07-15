588af945cdccec2adeb3d2a326c4e4c1e4e6c59b
Berlin Sky <pedroyoung8989@gmail.com>
2018-07-14 17:02:48 -0700

    Animation Implementation

--- src/App.vue
+++ src/App.vue
@@ -1,74 +1,29 @@
 <template>
   <div id="app">
     <img src="./assets/logo.png">
-    <h1>{{ msg }}</h1>
-    <h2>Fancy Button</h2>
 
-    <fancy-button @buttonClicked="eventListenner($event)" button-title="Not so fancy Button"></fancy-button>
+    <transition name="fadeIn"
+      enter-active-class="animated fadeIn"
+      leave-active-class="animated fadeOut">
 
-    <shopping-input></shopping-input>
-    <shopping-list :shoppingList="shoppingList"></shopping-list>
-    <button>Static Button</button>
+      <h1 v-if="toggle">I fade in and out!</h1>
+    </transition>
 
-    <div>
-      <input type="text" v-model="message">
-      <button @click="sendMessage()">+</button>
-
-      <message v-for="message in messageList" :message-text="message.text" :key="message.text">
-        <h2 slot="date">{{ message.date | date }}</h2>
-      </message>
-    </div>
-
+    <button @click="toggle = !toggle">Toggle Heading</button>
   </div>
 </template>
 
 <script>
-import FancyButton from './components/FancyButton';
-
-import EventBus from './EventBus';
-import ShoppingInput from './components/ShoppingInput';
-import ShoppingList from './components/ShoppingList'
-
-import moment from 'moment';
-import Message from './components/Message';
-
-const convertDateToString = value => moment(String(value)).format("dddd, MMMM Do YYYY");
-
 export default {
   name: 'app',
-  components: {
-    FancyButton,
-    ShoppingInput,
-    ShoppingList,
-    Message
-  },
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App',
-      shoppingList: [],
-      message: '',
-      messageList: [],
+      toggle: false
     }
-  },
-  created() {
-    EventBus.$on('addShoppingItem', (itemName) => {
-      this.shoppingList.push(itemName);
-    })
   },
   methods: {
-    eventListenner(message) {
-      console.log("The button was clicked from the child component with this message: " + message);
-    },
-    sendMessage() {
-      if (this.message.length >0) {
-        this.messageList.push({date: new Date(), text: this.message});
-        console.log(this.message);
-        this.message = '';
-      }
-    }
   },
   filters: {
-    date: convertDateToString
   }
 }
 </script>
