12c2ab2223337dde44aa988f0569323d8be755fd
Berlin Sky <pedroyoung8989@gmail.com>
2018-07-19 18:04:52 -0700

    updates

--- src/main.js
+++ src/main.js
@@ -7,10 +7,12 @@ Vue.use(VueRouter);
 
 const appOne = { template: `<h1>Eins</h1>`};
 const appTwo = { template: `<h1>Zwei</h1>`};
+const appThree = { template: `<h1>Hello, {{ $route.params.name }}</h1>`};
 
 const routes = [
   { path: '/one', component: appOne },
-  { path: '/two', component: appTwo }
+  { path: '/two', component: appTwo },
+  { path: '/three/:name', component: appThree },
 ]
 
 const router = new VueRouter({
