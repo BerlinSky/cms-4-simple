b57f6f4684f0010137041464135348d4110fb372
Berlin Sky <pedroyoung8989@gmail.com>
2018-07-19 17:44:52 -0700

    updates

--- src/main.js
+++ src/main.js
@@ -1,5 +1,21 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import VueRouter from 'vue-rouer';
+
+import App from './App.vue';
+
+Vue.use(VueRouter);
+
+const appOne = { template: `<h1>Eins</h1>`};
+const appTwo = { template: `<h1>Zwei</h1>`};
+
+const routes = [
+  { path: '/one', component: appOne },
+  { path: '/two', component: appTwo }
+]
+
+const router = new VueRouter({
+  routes
+})
 
 new Vue({
   el: '#app',
